d6d8506438513b50a40b04c3c91445636f004776
Guillaume ROTH <mail.guillaume.roth@gmail.com>
2020-12-17 23:13:20 +0100

    correction lanching

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
  // npm init
- // npm install --save-exact express@4.17.1 cors@2.8.5 mysql@2.17.1
-
+ // npm install --save-exact express@4.17.1 cors@2.8.5 mysql@2.17.1 multer@1.4.2 file-extension
+ 
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
