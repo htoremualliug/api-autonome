da8ffe34357a3cc9df2d17a346ace18e86a1c599
Guillaume ROTH <mail.guillaume.roth@gmail.com>
2020-12-24 02:28:57 +0100

    version démo autonome

--- src/index.js
+++ src/index.js
@@ -25,10 +25,27 @@ const port = 3000;
 
 const app = express();
 
-app.options('*', cors()) // include before other routes 
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app
-  .use(cors())
+  //.use(cors())
   .use(bodyParser.json())
   .use(events(connection))
   .use(upload());
